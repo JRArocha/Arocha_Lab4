1e244ede49f401ebcc54e5fa53ef585065cb8956
JR Arocha <johnroche.arocha@cvsu.edu.ph>
2022-11-12 19:49:59 +0800

    Update

--- GrabPanda-app/routes/web.php
+++ GrabPanda-app/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Project;
 
+Route::get('/', [Project::class, 'login']);
 Route::get('/login', [Project::class, 'login']);
 Route::get('/fetch_login', [Project::class, 'fetch_login']);
 Route::get('/logout', [Project::class, 'logout']);
